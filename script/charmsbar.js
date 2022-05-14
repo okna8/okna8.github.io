a84d22f821a15e0cf99fb908f7570bf8f97043fc
tozor.inc <99423393+okna8@users.noreply.github.com>
2022-05-14 14:20:42 +0300

    Add files via upload

---
+++ script/charmsbar.js
@@ -0,0 +1,22 @@
+/*$(function(){
+    $('.charmsbarBtn_settengs').click(function(){
+        $('.charmsbar_settengs').css('display', 'block');
+        $('.charmsbarpanelbg').css('display', 'block');
+    });
+    $('.charmsbarpanelbg').click(function(){
+        $('.charmsbar_settengs').css('display', 'none');
+        $('.charmsbarpanelbg').css('display', 'none');
+    });
+  });
+
+$(function(){
+    $('.charmsbarBtn_settengs_personalize').click(function(){
+        $('.charmsbar_settengs').css('display', 'none');
+        $('.charmsbar_settengs_personalize').css('display', 'block');
+        $('.charmsbarpanelbg').css('display', 'block');
+    });
+    $('.charmsbarpanelbg').click(function(){
+        $('.charmsbar_settengs_personalize').css('display', 'none');
+        $('.charmsbarpanelbg').css('display', 'none');
+    });
+});*/
